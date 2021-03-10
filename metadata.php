5a65ae87094b294d0ffae869ff6b71b1b3806008
nitinsingh_oxidesales <nitin.singh@oxid-esales.com>
2021-03-10 19:17:24 +0100

    Update ConfigImport

--- metadata.php
+++ metadata.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of OXID Module Configuration Im-/Exporter module.
  *
@@ -24,6 +25,9 @@
  * @copyright (C] OXID eSales AG 2003-2014
  */
 
+use OxidProfessionalServices\ModulesConfig\Controller\Admin\ShopMain;
+use OxidProfessionalServices\ModulesConfig\Controller\Admin\Dashboard;
+
 /**
  * Metadata version
  */
@@ -45,10 +49,10 @@ $aModule = [
     'url'         => 'http://www.oxid-esales.com',
     'email'       => 'info@oxid-esales.com',
     'extend'      => [
-        'shop_main'  => OxidProfessionalServices\ModulesConfig\Controller\Admin\ShopMain::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\ShopMain::class  => ShopMain::class,
     ],
     'controllers' => [
-        'admin_oxpsmodulesconfigdashboard'  => OxidProfessionalServices\ModulesConfig\Controller\Admin\Dashboard::class,
+        'admin_oxpsmodulesconfigdashboard'  => Dashboard::class,
     ],
     'templates'   => [
         'admin_oxpsmodulesconfigdashboard.tpl' => 'oxps/modulesconfig/views/admin/admin_oxpsmodulesconfigdashboard.tpl',
